3167ff2370c1d15ee2c37a783b75435758d36c04
Ramya Nagarajan <ramyanaga.99@gmail.com>
2020-07-14 17:24:10 -0400

    db functions

--- ramyatestdb.py
+++ ramyatestdb.py
@@ -1,4 +1,14 @@
+import psycopg2
+import json
+import binascii
+import base64
+from datetime import datetime
+from seal import *
+from seal_helper_outer import *
+
+
 def storeComputeResult(result, documentId, timestamp, computationType):
+    print("in storeComputeResult, crypto_server/ramyatestdb.py")
     conn = psycopg2.connect(database="crypto_db", user="postgres", password="", host = "127.0.0.1", port = "5432")
     print("Opened database successfully")
     cur = conn.cursor()
@@ -24,35 +34,35 @@ def getComputationResult(documentId):
         compute_result, documentId, timestamp, computeType = row[0], row[1], row[2], row[3]
         return bytes.fromhex(compute_result)
 
-keygen = KeyGenerator(context)
-public_key = keygen.public_key()
-secret_key = keygen.secret_key()
-encryptor = Encryptor(context, public_key)
-evaluator = Evaluator(context)
-decryptor = Decryptor(context, secret_key)
-encoder = CKKSEncoder(context)
-value1 = 5
-plain1 = Plaintext()
-encoder.encode(value1, scale, plain1)
-encrypted1 = Ciphertext()
-encryptor.encrypt(plain1, encrypted1)
-encrypted1.save("encrypted_result_db")
-with open("encrypted_result_db", "rb") as f:
-    encrypted_result_bytes = f.read()
+# keygen = KeyGenerator(context)
+# public_key = keygen.public_key()
+# secret_key = keygen.secret_key()
+# encryptor = Encryptor(context, public_key)
+# evaluator = Evaluator(context)
+# decryptor = Decryptor(context, secret_key)
+# encoder = CKKSEncoder(context)
+# value1 = 5
+# plain1 = Plaintext()
+# encoder.encode(value1, scale, plain1)
+# encrypted1 = Ciphertext()
+# encryptor.encrypt(plain1, encrypted1)
+# encrypted1.save("encrypted_result_db")
+# with open("encrypted_result_db", "rb") as f:
+#     encrypted_result_bytes = f.read()
 
-encrypted_result_hex = encrypted_result_bytes.hex()
-documentId = "3"
-time = datetime.utcnow()
-computationType = "test"
-storeComputeResult(encrypted_result_hex, documentId, time, computationType)
+# encrypted_result_hex = encrypted_result_bytes.hex()
+# documentId = "3"
+# time = datetime.utcnow()
+# computationType = "test"
+# storeComputeResult(encrypted_result_hex, documentId, time, computationType)
 
-result_bytes = getComputationResult(documentId)
-with open("load_encrypted_result", "wb") as f:
-    f.write(result_bytes)
+# result_bytes = getComputationResult(documentId)
+# with open("load_encrypted_result", "wb") as f:
+#     f.write(result_bytes)
 
-load_result = Ciphertext()
-load_result.load(context, "load_encrypted_result")
-plain_result = Plaintext()
-decryptor.decrypt(load_result, plain_result)
-output = DoubleVector()
-encoder.decode(plain_result, output)
+# load_result = Ciphertext()
+# load_result.load(context, "load_encrypted_result")
+# plain_result = Plaintext()
+# decryptor.decrypt(load_result, plain_result)
+# output = DoubleVector()
+# encoder.decode(plain_result, output)
